117ef7f1b30421a13373bcffe443c23ae81977c0
ahsanul0185 <ahsanulhaque.dev@gmail.com>
2024-11-29 18:55:05 +0600

    added corsOptions in server.js

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,26 @@ import orderRouter from './routes/orderRouter.js'
 const app = express()
 const port = process.env.PORT || 4000
 
+
+// CORS configuration
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        // Allow requests from the frontend or if there's no origin (e.g., from Postman)
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+  };
+
+
+
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // api endpoints
 app.use('/api/user', userRouter);
